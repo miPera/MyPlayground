9a89426d61099c9f468d23e72a01b4becf7d425f
miPera <migperalta10@gmail.com>
2017-08-09 20:16:41 -0700

    Initial commit

---
+++ MyPlayground.playground/Contents.swift
@@ -0,0 +1,8 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+//This playground is used to experiment with Swift standard types
+
+//variables can change values
+var str = "Hello, playground"
