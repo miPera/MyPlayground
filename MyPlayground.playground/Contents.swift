a024e7f9b4a66534b649324ae0ef6013b33c146b
miPera <migperalta10@gmail.com>
2017-08-09 20:32:24 -0700

    Added variables and constants with comments

--- MyPlayground.playground/Contents.swift
+++ MyPlayground.playground/Contents.swift
@@ -2,7 +2,25 @@
 
 import UIKit
 
-//This playground is used to experiment with Swift standard types
+// This playground is used to experiment with Swift standard types
 
-//variables can change values
+//------------------------
+// Variables and Inference:
+//------------------------
+/*
+ Type Inference: The compiler infers the values for the variables and constants below using initial value
+ Option-click on the variable or constant identifier to view the inferred type
+ */
+
+// variables "var" can change values anytime after declaration
 var str = "Hello, playground"
+str = "Hello, Swift"
+
+// "let" denotes a constant value, which cannot be changed
+let constStr = str
+//constStr = "Hello World"    //causes an error since constStr is a constant
+
+//The following specify the variable type (no type inference)
+var nextYear: Int
+var bodyTemp: Float
+var hasPet: Bool
